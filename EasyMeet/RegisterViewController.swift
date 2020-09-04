ddbcf9a0a862916ed2fe015923cb8fcdbbf986e2
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2020-09-04 17:45:47 +0900

    #2020.09.04 Showing error if data is not inputed

--- EasyMeet/RegisterViewController.swift
+++ EasyMeet/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ProgressHUD
 
 class RegisterViewController: UIViewController {
 
@@ -17,25 +18,54 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var genderSegmentOutlet: UISegmentedControl!
+    @IBOutlet weak var backgroundImageView: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        overrideUserInterfaceStyle = .dark
+        setupBackgroundTouch()
+        
     }
     
     //MARK: - IBActions
     
     
     @IBAction func backButtonPressed(_ sender: Any) {
-        
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func registerButtonPressed(_ sender: Any) {
-        
+        if isTextDataInputed() {
+            //register the user
+        } else {
+            ProgressHUD.showError("All fields are required!")
+        }
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //Mark: - Setup
+    private func setupBackgroundTouch() {
+        backgroundImageView.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
+        backgroundImageView.addGestureRecognizer(tapGesture)
         
+    }
+    
+    @objc func backgroundTap() {
+        dismissKeyboard()
+    }
+    
+    //Mark: - Helpers
+    private func dismissKeyboard() {
+        self.view.endEditing(false)
+    }
+    
+    private func isTextDataInputed() -> Bool {
+        return usernameTextField.text != "" && emailTextField.text != "" && cityTextField.text != "" && dateOfBirthTextField.text != "" && passwordTextField.text != "" && confirmPasswordTextField.text != ""
     }
 }
--- EasyMeet/WelcomeViewController.swift
+++ EasyMeet/WelcomeViewController.swift
@@ -6,23 +6,61 @@
 //
 
 import UIKit
+import ProgressHUD
 
 class WelcomeViewController: UIViewController {
     
     //MARK: - IBOutlets
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var backgroundImageView: UIImageView!
     
     //MARK: - ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        overrideUserInterfaceStyle = .dark
+        
+        setupBackgroundTouch()
 
     }
     //MARK: - IBActions
     
     @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
+        
+        if emailTextField.text != "" {
+            //reset password
+        } else {
+            //show error
+            ProgressHUD.showError("Please insert your email address.")
+        }
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            //login
+        } else {
+            //show error
+            ProgressHUD.showError("All fields are required")
+        }
     }
+    
+    //Mark: - Setup
+    
+    private func setupBackgroundTouch() {
+        backgroundImageView.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
+        backgroundImageView.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    @objc func backgroundTap() {
+        dismissKeyboard()
+    }
+    
+    //Mark: - Helpers
+    private func dismissKeyboard() {
+        self.view.endEditing(false)
+    }
+    
 }
